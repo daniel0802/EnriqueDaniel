94f038d1dafab70eb4de2c98f52e1b09994faffa
Enrique-GD <enryares@gmail.com>
2016-05-12 10:08:34 +0200

    Version 1.1 Comentado el metodo multiplicacion

--- Operaciones.java
+++ Operaciones.java
@@ -2,12 +2,19 @@
 /**
  * Se trata de un programa desarrollado en <b>JAVA</b>que permite realizar una multiplicación
  * @author Daniel y Henrique
- * @version v1.0
+ * @version v1.1
  * @since v1.0
  *
  */
 public class Operaciones {
 
+	
+	/**
+	 * @param n1 Primer numero
+	 * @param n2 Segundo numero
+	 * @return op Es el resulktado de la multiplicacion de n1 y n2
+	 * @since 1.1
+	 */
 	public int multiplicacion(int n1, int n2){
 		int op;
 		op = n1 * n2;
