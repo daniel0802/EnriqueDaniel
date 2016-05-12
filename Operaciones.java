e45eef1545e32b3a8f14bf361a42d87b2540044d
jsalor <jorgesalor24@gmail.com>
2016-05-12 09:59:33 +0200

    Primer comentario Javadoc

---
+++ Operaciones.java
@@ -0,0 +1,20 @@
+
+/**
+ * Se trata de un programa desarrollado en <b>JAVA</b>que permite realizar una multiplicación
+ * @author Daniel y Henrique
+ * @version v1.0
+ * @since v1.0
+ *
+ */
+public class Operaciones {
+
+	public int multiplicacion(int n1, int n2){
+		int op;
+		op = n1 * n2;
+		
+		return op;
+	}
+	
+	
+	
+}
